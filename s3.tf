713aee5e73817df831b05c71773b28c1bd02bccb
Ahmed Nuaman <ahmed.nuaman@firemind.io>
2021-12-09 14:14:18 +0000

    changed to use bucket_prefix

--- s3.tf
+++ s3.tf
@@ -1,11 +1,6 @@
-resource "random_string" "hash" {
-  length  = 16
-  special = false
-}
-
 resource "aws_s3_bucket" "redirect_bucket" {
-  bucket = "redirect-${var.zone}-${lower(random_string.hash.result)}"
-  acl    = "public-read"
+  bucket_prefix = "redirect-${var.zone}"
+  acl           = "public-read"
 
   website {
     redirect_all_requests_to = var.target_url
